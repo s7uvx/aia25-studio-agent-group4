16abdfbc89c45113cde16598923e5466848a6af8
joaomigsilva <35776833+jomiguelcarv@users.noreply.github.com>
2025-04-27 13:48:10 +0100

    readme changes

---
+++ server/gh_server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from config import *
+from llm_calls import *
+
+app = Flask(__name__)
+
+
+@app.route('/llm_call', methods=['POST'])
+def llm_call():
+    data = request.get_json()
+    input_string = data.get('input', '')
+
+    answer = classify_input(input_string)
+
+    return jsonify({'response': answer})
+
+if __name__ == '__main__':
+    app.run(debug=True)
